b4d00cabdafab5d0ed6a8ede247443281cbe57a3
VitoLattanzi <vitofrancolattanzi@gmail.com>
2026-09-16 21:49:55 -0300

    feat: fix passwords errors

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -18,6 +18,7 @@ export default function LoginPage() {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
 
